5e19050a157a351f929672e8aac9340747628a28
Cameron Abel <cameronabel@gmail.com>
2022-12-04 10:16:34 -0800

    add basic Player structure
    Co-authored-by: Tyler Quinn <tyler.quinn032@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
 function rollDie () {
-    return Math.floor(Math.random() * 6) + 1;
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+class Player{
+  constructor() {
+    this.score = 0;
+    this.currentTurn = [];
+  }
+
+  takeTurn() {
+    let roll = rollDie();
+    if (roll === 1) {
+      return 0;
+    } else {
+      this.currentTurn.push(roll);
+    }
+  }
+
+  hold() {
+    this.score += this.currentTurn.reduce((a, b) => a + b)
+    this.current = [];
+  }
 }
