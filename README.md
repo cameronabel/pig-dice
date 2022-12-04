349b011e2df58f01dc385152ccff4cb1d4b5dd02
Cameron Abel <cameronabel@gmail.com>
2022-12-04 11:30:17 -0800

    add game object and started game logic
    Co-authored-by: Tyler Quinn <tyler.quinn032@gmail.com>

--- README.md
+++ README.md
@@ -5,3 +5,30 @@
 **Code:**\
 rollDie();\
 **Expected Output: "<random integer between 1 and 6>"**
+
+## Describe: Player().takeTurn()
+
+#### Test: It should update the currentTurn array with the rolled value
+
+**Code:**\
+Player().takeTurn();\
+**Expected Output: none, internal array is updated**
+
+#### Test: It should reset the internal array to empty if a 1 is rolled
+
+**Code:**\
+Player().takeTurn();\
+**Expected Output: none, internal array is reset**
+
+## Describe: Player().hold()
+
+#### Test: It should update the Player's score and reset the internal array to empty
+
+**Code:**\
+player = new Player();\
+player.currentTurn = [1, 2, 3];\
+player.hold();\
+player.score;\
+player.currentTurn;\
+**Expected Output: 6**
+**[]**
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Business Logic
 function rollDie () {
   return Math.floor(Math.random() * 6) + 1;
 }
@@ -10,15 +11,73 @@ class Player{
 
   takeTurn() {
     let roll = rollDie();
+    console.log(roll);
     if (roll === 1) {
-      return 0;
+      this.currentTurn = [];
     } else {
       this.currentTurn.push(roll);
     }
+    return roll;
   }
 
   hold() {
-    this.score += this.currentTurn.reduce((a, b) => a + b)
-    this.current = [];
+    this.score += this.currentTurn.reduce((a, b) => a + b);
+    this.currentTurn = [];
   }
+}
+
+class Game {
+  constructor() {
+    this.players = [new Player(), new Player()];
+    this.activePlayerIndex = 0;
+  }
+  swapPlayer() {
+    this.activePlayerIndex += 1;
+    this.activePlayerIndex = this.activePlayerIndex % 2;
+  }
+  activePlayer() {
+    return this.players[this.activePlayerIndex];
+  }
+}
+
+
+function pigDice() {
+  
+  let activePlayer = game.activePlayer();
+
+}
+  
+
+
+// Interface Logic
+const game = new Game();
+
+function rollButtonHandler() {
+  event.preventDefault();
+  let roll = game.activePlayer().takeTurn();
+  if (roll === 1) {
+    game.swapPlayer();
+  }
+  // if roll was 1, call another func to swap players
+  console.log(game.activePlayer().score); // update score display
+  console.log(game.activePlayer().currentTurn); // update current roll display
+}
+
+function holdButtonHandler(event) {
+  event.preventDefault();
+  game.activePlayer().hold();
+  game.swapPlayer();
+  // swap the display to the other player
+  // check for 100 pts
+}
+
+
+window.onload = function () {
+  const rollButton = document.getElementById('roll');
+  rollButton.addEventListener("submit", rollButtonHandler);
+
+  const holdButton = document.getElementById('hold');
+  holdButton.addEventListener("submit", holdButtonHandler);
+
+  pigDice();
 }
