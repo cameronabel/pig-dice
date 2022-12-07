981f946d2f25bd217291cb3807585fafc1805a3a
Cameron Abel <cameronabel@gmail.com>
2022-12-06 20:25:59 -0800

    end page support and rematch/new game buttons
    Co-authored-by: Tyler Quinn <tyler.quinn032@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -35,8 +35,13 @@ class Game {
     this.activePlayerIndex = 0;
   }
   swapPlayer() {
+    document.getElementById(game.activePlayer().playerField).classList.remove('highlighted');
+    document.getElementById(game.activePlayer().playerField).classList.add('not-highlighted');
     this.activePlayerIndex += 1;
     this.activePlayerIndex = this.activePlayerIndex % 2;
+    document.getElementById(game.activePlayer().playerField).classList.add('highlighted');
+    document.getElementById(game.activePlayer().playerField).classList.remove('not-highlighted');
+   
   }
   activePlayer() {
     return this.players[this.activePlayerIndex];
@@ -83,8 +88,8 @@ function holdButtonHandler() {
   game.activePlayer().hold();
   document.getElementById(game.activePlayer().scoreField).innerText= game.activePlayer().score;
   clearDisplays();
-  if (game.activePlayer().score >= 100) {
-    return // Game over
+  if (game.activePlayer().score >= 10) {
+    endPageHandler();
   } else {
   game.swapPlayer();
   }
@@ -95,6 +100,8 @@ function startPageHandler() {
   event.preventDefault();
   const player1Name = document.getElementById('player1').value || 'Player 1';
   const player2Name = document.getElementById('player2').value || 'Player 2';
+  document.getElementById('player1').value = "";
+  document.getElementById('player2').value = "";
   document.getElementById('start-page').classList.add('hidden');
   document.getElementById('game-play').classList.remove('hidden');
   game.players[0].name = player1Name;
@@ -105,6 +112,41 @@ function startPageHandler() {
   document.getElementById('p2-name').innerText= player2Name + "'s" + ' Score:';
   document.getElementById(game.players[0].scoreField).innerText= 0;
   document.getElementById(game.players[1].scoreField).innerText= 0;
+  game.players[0].playerField = 'player1-score';
+  game.players[1].playerField = 'player2-score';
+  document.getElementById(game.players[0].playerField).classList.remove('not-highlighted');
+  document.getElementById(game.players[0].playerField).classList.add('highlighted');
+
+}
+
+function endPageHandler() {
+  document.getElementById('start-page').classList.add('hidden');
+  document.getElementById('game-play').classList.add('hidden');
+  document.getElementById('game-over').classList.remove('hidden');
+  document.getElementById('final-score').innerText = game.activePlayer().score;
+  document.getElementById('final-score').classList.add('spinner');
+  document.getElementById('winner').innerText = `${game.activePlayer().name} wins!`
+  const rematchButton = document.getElementById('rematch');
+  rematchButton.addEventListener("click", rematchHandler);
+  const newGameButton = document.getElementById('new-game');
+  newGameButton.addEventListener("click", newGameHandler);
+}
+
+function newGameHandler() {
+  document.getElementById('start-page').classList.remove('hidden');
+  document.getElementById('game-play').classList.add('hidden');
+  document.getElementById('game-over').classList.add('hidden');
+  game.players[0].score = 0;
+  game.players[1].score = 0;
+}
+
+function rematchHandler() {
+  document.getElementById('game-play').classList.remove('hidden');
+  document.getElementById('game-over').classList.add('hidden');
+  document.getElementById(game.players[0].scoreField).innerText= 0;
+  document.getElementById(game.players[1].scoreField).innerText= 0;
+  game.players[0].score = 0;
+  game.players[1].score = 0;
 }
 
 window.onload = function () {
